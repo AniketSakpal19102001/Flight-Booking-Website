75c91eae4f29c7c11db2d3857978693dfbce81ce
Aniket <sakpalaniket44@gmail.com>
2025-01-05 04:58:11 +0530

    added api/ in frontend

--- frontend/src/redux/constant.js
+++ frontend/src/redux/constant.js
@@ -1,5 +1,5 @@
 export const BASE_URL = import.meta.env.VITE_API_URL;
-export const USER_URL = "/auth";
-export const FLIGHT_URL = "/flights";
-export const BOOKING_URL = "/bookings";
-export const TICKET_URL = "/tickets";
+export const USER_URL = "api/auth";
+export const FLIGHT_URL = "api/flights";
+export const BOOKING_URL = "api/bookings";
+export const TICKET_URL = "api/tickets";
